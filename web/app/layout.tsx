dc44af3b86a7a63c38d152a478d16656b362a068
Vikash Mall <vikashmall4242@gmail.com>
2025-10-19 00:55:05 -0500

    frtonend slop 3

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -20,18 +20,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Titillium+Web:wght@400;700&display=swap"
-          rel="stylesheet"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Orbitron:wght@700&display=swap"
-          rel="stylesheet"
-        />
-      </head>
       <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
