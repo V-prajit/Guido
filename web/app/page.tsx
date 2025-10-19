66d7bcba4d2428e9f19fe76a3dd26f27f67ab1e5
Vikash Mall <vikashmall4242@gmail.com>
2025-10-19 04:26:53 -0500

    add padding

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -4,7 +4,7 @@ import { Box1, Box2, Box3, Box4, Box5, Box6, Box7, Box8 } from './components/Box
 
 export default function Home() {
   return (
-    <main className="flex h-screen w-screen overflow-hidden">
+    <main className="flex h-screen w-screen overflow-hidden p-4">
       <div
         className="grid h-full w-full gap-6"
         style={{
